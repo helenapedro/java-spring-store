305223f95ce7f6b63c8756ddebe6f2ab7fdb7b0c
Helena Pedro <mbeua94@gmail.com>
2025-03-04 16:44:32 +0100

    new commit

--- src/main/java/com/pedro/store/services/StripePaymentService.java
+++ src/main/java/com/pedro/store/services/StripePaymentService.java
@@ -1,15 +1,25 @@
 package com.pedro.store.services;
 
 import com.pedro.store.services.PaymentService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service("stripe")
 @Primary
 public class StripePaymentService implements PaymentService {
+    @Value("${stripe.apiUrl}")
+    private String apiUrl;
+
+    @Value("${tripe.supported-currencies}")
+    private List<String> supportedCurrencies;
+
     @Override
     public void processPayment(double amount) {
         System.out.println("STRIPE");
+        System.out.println("Currencies" + supportedCurrencies);
         System.out.println("Amount: " + amount);
     }
 }
